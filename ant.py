c978ac057b00708f1a87324a869ebdb19320465e
Mqtth3w <mqtth3w@duck.com>
2024-11-15 20:32:38 +0100

    Add files via upload

---
+++ ant.py
@@ -0,0 +1,34 @@
+'''
+@author Mqtth3w https://github.com/Mqtth3w
+@license GPL-3.0
+'''
+
+_N = 10
+_seed = 1
+
+class Ant:
+    def __init__(self, N, m, x, y):
+        self._x = x
+        self._y = y
+        self._score = 0
+        self._total_score = 0
+        self._N = N
+        self._m = m
+        self._board = None
+    
+    def ant_move(self, x, y):
+        pass
+           
+     
+        
+def ant_rec(self, N , m, filename, seed):
+    pass
+
+
+def ant_train(self, seed):
+    pass   
+        
+        
+        
+        
+
